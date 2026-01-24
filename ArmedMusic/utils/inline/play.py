96e9642ad8c19d23324ff7ea67c5b3762f84d4b6
sqenaah <sqenaah@github.com>
2026-01-24 23:02:23 +0400

    update

--- ArmedMusic/utils/inline/play.py
+++ ArmedMusic/utils/inline/play.py
@@ -15,23 +15,23 @@ def stream_markup_timer(_, chat_id, played, dur):
         percentage = min(played_sec / duration_sec * 100, 100)
     umm = math.floor(percentage)
     if umm <= 0:
-        bar = '⬤─────────'
+        bar = '⬤━━━━━━━━━'
     elif umm <= 10:
-        bar = '━⬤────────'
+        bar = '━⬤━━━━━━━━'
     elif umm < 20:
-        bar = '━━⬤───────'
+        bar = '━━⬤━━━━━━━'
     elif umm < 30:
-        bar = '━━━⬤──────'
+        bar = '━━━⬤━━━━━━'
     elif 40 <= umm < 50:
-        bar = '━━━━⬤─────'
+        bar = '━━━━⬤━━━━━'
     elif 50 <= umm < 60:
-        bar = '━━━━━⬤────'
+        bar = '━━━━━⬤━━━━'
     elif 60 <= umm < 70:
-        bar = '━━━━━━⬤───'
+        bar = '━━━━━━⬤━━━'
     elif 70 <= umm < 80:
-        bar = '━━━━━━━⬤──'
+        bar = '━━━━━━━⬤━━'
     elif 80 <= umm < 95:
-        bar = '━━━━━━━━⬤─'
+        bar = '━━━━━━━━⬤━'
     else:
         bar = '━━━━━━━━━⬤'
     buttons = [[InlineKeyboardButton(text=f"{played} {bar} {dur}", callback_data='GetTimer')], [InlineKeyboardButton(text='ᐖ', callback_data=f"ADMIN Resume|{chat_id}"), InlineKeyboardButton(text='II', callback_data=f"ADMIN Pause|{chat_id}"), InlineKeyboardButton(text='⟳', callback_data=f"ADMIN Replay|{chat_id}"), InlineKeyboardButton(text='▷▷', callback_data=f"ADMIN Skip|{chat_id}"), InlineKeyboardButton(text='▢', callback_data=f"ADMIN Stop|{chat_id}")]]
