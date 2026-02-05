a5b056dbdf89d313b5c083441533c9b896675e40
kaprenqktenanq-cloud <kaprenqktenanq@gmail.com>
2026-02-05 23:31:47 +0000

    Replace queue 'Added to Queue' message buttons: show only close button instead of playback controls

--- ArmedMusic/utils/inline/queue.py
+++ ArmedMusic/utils/inline/queue.py
@@ -12,5 +12,5 @@ def queue_back_markup(_, CPLAY):
     return upl
 
 def aq_markup(_, chat_id):
-    buttons = [[InlineKeyboardButton(text='ᐖ', callback_data=f'ADMIN Resume|{chat_id}'), InlineKeyboardButton(text='II', callback_data=f'ADMIN Pause|{chat_id}'), InlineKeyboardButton(text='▷▷', callback_data=f'ADMIN Skip|{chat_id}'), InlineKeyboardButton(text='▢', callback_data=f'ADMIN Stop|{chat_id}')]]
+    buttons = [[InlineKeyboardButton(text=_['CLOSE_BUTTON'], callback_data='close')]]
     return buttons
